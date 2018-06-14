8b8374f38be9329ea7f8fe824d0d5f1f2240281f
PinkDeer <bunman@mail.ru>
2018-06-14 14:47:18 +0300

    Added scaffold photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_13_075157) do
+ActiveRecord::Schema.define(version: 2018_06_14_113536) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string "descpription"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
