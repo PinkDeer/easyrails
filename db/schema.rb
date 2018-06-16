a19cbcdde4aff5c12bebd8f7d9552f239b5350a9
PinkDeer <bunman@mail.ru>
2018-06-16 09:41:45 +0300

    Set up associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_14_113536) do
+ActiveRecord::Schema.define(version: 2018_06_16_063306) do
 
   create_table "photos", force: :cascade do |t|
     t.string "descpription"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
