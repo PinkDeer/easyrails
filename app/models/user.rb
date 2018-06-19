ccd267f3f88d50645fabfaa1c8ad04469832b3f0
PinkDeer <bunman@mail.ru>
2018-06-19 16:29:05 +0300

    Added validation gor user and photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :name, presence: true
+
   has_many :photos
   has_many :subscriptions
+
 end
