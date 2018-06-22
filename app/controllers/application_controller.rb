6f931d6964d78a3dfec3df0f059a7d747a07044d
PinkDeer <bunman@mail.ru>
2018-06-22 16:21:23 +0300

    Started localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = extract_locale_from_accept_language_header
+  end
+
+  def extract_locale_from_accept_language_header
+
+   http_accept_language = request.env['HTTP_ACCEPT_LANGUAGE']
+
+   if http_accept_language
+      case http_accept_language.scan(/^[a-z]{2}/).first
+         when 'en'
+                  'en'
+         when 'ru'
+                  'ru'
+         else
+                  'ru'
+         end
+      else
+         'ru'
+      end
+end
 
   protected
 
